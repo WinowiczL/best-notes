79369dfa53bd9e18bf623464d775442356e605db
Lukasz Winowicz <37307177+WinowiczL@users.noreply.github.com>
2020-03-16 21:52:05 +0100

    ThemeProvider added

--- src/components/atoms/Button/Button.stories.js
+++ src/components/atoms/Button/Button.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { Button } from '../Button/Button';
+import { Button } from './Button';
 import { withKnobs, select } from '@storybook/addon-knobs'
 
 storiesOf('Button', module)
--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Button } from '../../components/Button/Button';
+import { ThemeProvider } from 'styled-components';
+import { Button } from '../../components/atoms/Button/Button';
 import { GlobalStyle } from '../../theme/GlobalStyle';
+import { theme } from "../../theme/mainTheme";
 
 export const Root = () => (
   <div>
     <GlobalStyle />
-    <h1>Root</h1>
-    <Button>Close / Save</Button>
-    <Button secondary>Remove</Button>
+    <ThemeProvider theme={theme}>
+      <>
+        <h1>Root</h1>
+        <Button>Close / Save</Button>
+        <Button secondary>Remove</Button>
+      </>
+    </ThemeProvider>
   </div>
 );
